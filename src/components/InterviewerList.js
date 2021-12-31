abe8790323ec391007ab226336346198b5f17a41
Daniel Wajsman <danielwajsman@mac.com>
2021-12-30 23:15:17 -0800

    interviews now save and edit

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -4,9 +4,9 @@ import InterviewerListItem from "components/InterviewerListItem";
 import PropTypes from "prop-types";
 
 export default function InterviewerList(props) {
-      console.log("💩",props.interviewers)
+      console.log("💩 InterviewerList",props.interviewers)
   const interviewersList = props.interviewers.map((interviewer) =>  
-    
+      // console.log("in map",interviewer)
       <InterviewerListItem
       key={interviewer.id}
       id={interviewer.id}
@@ -23,7 +23,7 @@ export default function InterviewerList(props) {
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list"> 
-        {/* {interviewersList} */}
+        {interviewersList}
       </ul>
       </section>
   )
